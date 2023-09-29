c95f4c23270b3625e40d8f415636dc930736a3c1
PedroKKr <52111108+PedroKKr@users.noreply.github.com>
2023-09-29 15:56:04 -0300

    updated

--- contador/days.js
+++ contador/days.js
@@ -1,7 +1,15 @@
 const startDate = new Date('2019-04-12T00:00:00'); 
+
 setInterval(() => {
     const now = new Date();
     const differenceInTime = now.getTime() - startDate.getTime(); 
     const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24)); 
-    document.getElementById('days').textContent = differenceInDays;
+    const years = Math.floor(differenceInDays / 365);
+    const months = Math.floor((differenceInDays % 365) / 30);
+    const days = Math.floor((differenceInDays % 365) % 30);
+
+    document.getElementById('totalDays').textContent = differenceInDays;
+    document.getElementById('years').textContent = years;
+    document.getElementById('months').textContent = months;
+    document.getElementById('days').textContent = days;
 }, 1000);
